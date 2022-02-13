e39fe898581a860881f9124d02687153c9950d15
Anushanallap <Anusha.n468@gmail.com>
2022-02-13 17:49:06 +0530

    creating addressbook service

---
+++ src/main/java/com/bridgelabz/addressspringapp/AddressSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressSpringAppApplication.class, args);
+    }
+
+}
