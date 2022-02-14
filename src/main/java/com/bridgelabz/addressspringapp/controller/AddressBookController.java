cdd8725104f3c3a8eee7df39ae281a980559be6a
Anushanallap <Anusha.n468@gmail.com>
2022-02-14 16:59:41 +0530

    adding regex patterns and Validation to Name,state,zipcode,phone ..

--- src/main/java/com/bridgelabz/addressspringapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressspringapp/controller/AddressBookController.java
@@ -4,13 +4,13 @@ package com.bridgelabz.addressspringapp.controller;
 import com.bridgelabz.addressspringapp.dto.AddressBookDTO;
 import com.bridgelabz.addressspringapp.dto.ResponseDTO;
 import com.bridgelabz.addressspringapp.model.AddressBookData;
-import com.bridgelabz.addressspringapp.services.AddressBookService;
 import com.bridgelabz.addressspringapp.services.IAddressBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,53 +21,49 @@ public class AddressBookController {
     @Autowired
     IAddressBookService addressBookService;
     
-    //http://localhost:8082/addressbookspring/details
-    @GetMapping(value = {"", "/", "/details"})
+    //http://localhost:8082/addressbookspring/get
+    @RequestMapping(value = {"","/","/get"})
     public ResponseEntity<ResponseDTO> getAddressBookData()
     {
-        AddressBookService addressBookServices = null;
-        List addressBookDataList = addressBookServices.getAddressBookData();
+        List addressBookDataList = addressBookService.getAddressBookData();
         ResponseDTO respDTO = new ResponseDTO("Get Call Success", addressBookDataList);
         return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
 
-    //http://localhost:8082/addressbookspring/details/1
-    @GetMapping("/details/{personId}")
+    //http://localhost:8082/addressbookspring/get/1
+    @GetMapping("/get/{personId}")
     public ResponseEntity<ResponseDTO> getAddressBookData(@PathVariable("personId") int personId)
     {
-        AddressBookService addressBookServices = null;
-        AddressBookData addressBookData = addressBookServices.getAddressBookDataById(personId);
+        AddressBookData addressBookData = addressBookService.getAddressBookDataById(personId);
         ResponseDTO respDTO = new ResponseDTO("Get Call By Id Success", addressBookData);
         return new ResponseEntity<ResponseDTO> (respDTO, HttpStatus.OK);
     }
 
     //http://localhost:8082/addressbookspring/create
     @PostMapping("/create")
-    public ResponseEntity<ResponseDTO> addAddressBookData(@RequestBody AddressBookDTO addressBookDTO)
+    public ResponseEntity<ResponseDTO> addAddressBookData(@Valid @RequestBody AddressBookDTO addressBookDTO)
     {
-        AddressBookService addressBookServices = null;
-        AddressBookData addressBookData = addressBookServices.createAddressBookData(addressBookDTO);
+        AddressBookData addressBookData = addressBookService.createAddressBookData(addressBookDTO);
         ResponseDTO respDTO = new ResponseDTO("Address book data created", addressBookData);
         return new ResponseEntity<ResponseDTO> (respDTO, HttpStatus.OK);
     }
 
     //http://localhost:8082/addressbookspring/update/1
+    @PutMapping("/update/{personId}")
     public ResponseEntity<ResponseDTO> updateAddressBookData(@PathVariable("personId") int personId,
                                                              @RequestBody AddressBookDTO addressBookDTO)
     {
-        AddressBookService addressBookServices = null;
-        AddressBookData addressBookData = addressBookServices.updateAddressBookData(personId, addressBookDTO);
-        ResponseDTO respDTO = new ResponseDTO("updated adressBook data", addressBookDTO);
+        AddressBookData addressBookData = addressBookService.updateAddressBookData(personId, addressBookDTO);
+        ResponseDTO respDTO = new ResponseDTO("Updated addressBook data", addressBookDTO);
         return new ResponseEntity<ResponseDTO> (respDTO, HttpStatus.OK);
     }
 
-
     //http://localhost:8082/addressbookspring/delete/1
+    @DeleteMapping("/delete/{personId}")
     public ResponseEntity<ResponseDTO> deleteAddressBookData(@PathVariable("personId") int personId)
     {
-        AddressBookService addressBookServices = null;
-        addressBookServices.deleteAddressBookData(personId);
-        ResponseDTO respDTO = new ResponseDTO("deleted adressBook data with personId :", personId);
+        addressBookService.deleteAddressBookData(personId);
+        ResponseDTO respDTO = new ResponseDTO("Deleted addressBook data with personId :", personId);
         return new ResponseEntity<ResponseDTO> (respDTO, HttpStatus.OK);
     }
 }
--- src/main/java/com/bridgelabz/addressspringapp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressspringapp/dto/AddressBookDTO.java
@@ -2,21 +2,32 @@ package com.bridgelabz.addressspringapp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 public @Data class AddressBookDTO {
 
-
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "FirstName name Invalid")
     public String FirstName;
 
+    @Pattern(regexp = "^[A-Z]{1}$", message = "LAstName name Invalid")
     public String LastName;
 
+    @Pattern(regexp = "Male|Female", message = "Gender should be Male or Female")
     public String Gender;
 
+    @Pattern(regexp = "[1-9]{1}[0-9]{9}", message = "Mobile number is invalid")
     public String PhoneNum;
 
+    @NotNull(message = "city can not be null")
     public String City;
 
+    @NotEmpty(message = "state can't be Empty")
     public String State;
 
+    @NotBlank(message = "zipcode should not be Empty")
     public String ZipCode;
 
     @Override
